cdd397a0574c1591ddcad2a0d17efb58b607f6a7
mariacpeixoto <mariacpeixoto18@gmail.com>
2023-04-14 22:40:34 -0300

    update

--- app.js
+++ app.js
@@ -11,7 +11,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
-  res.render("primeira_pagina");
+  res.render("Cadastro");
+});
+app.get("/consulta", function (req, res) {
+  post.findAll().then(function(post) {
+    res.render("consulta", {post})
+  }).catch(function(erro) {
+    console.log("Erro ao carregar os dados do banco: " + erro)
+  })
 });
 
 app.post("/cadastrar", function (req, res) {
@@ -24,7 +31,7 @@ app.post("/cadastrar", function (req, res) {
       observacao: req.body.observacao,
     })
     .then(function () {
-      res.send("Dados enviados com sucesso!");
+      res.redirect("/consulta")
     })
     .catch(function (erro) {
       res.send("Falha ao cadastrar os dados: " + erro);
